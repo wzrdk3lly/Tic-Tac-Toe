ee7729e00ed06449643f7ec6a741558cb248313c
wzrdk3lly <dkmk1149@gmail.com>
2022-08-20 16:55:48 -0400

    Create game board object

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log("we connected")
+//avoid global scope pls :)
+// create gameboard players as factory function 
+    // pass the gameboard to the player as a parameter when manipulating it(the game board)
+
+// 
+// create something to manually fill up the contents of the board when rendering in html
+
+const GameBoard = () => {
+    let body = document.getElementById("body")
+
+    
+    const board = () => {
+        return [" "," "," "," "," "," "," "," ",];
+    }
+
+    body.innerHTML = board()
+   
+
+    return {board}
+};
+
+let newBoard = GameBoard();
+
+
+
+
