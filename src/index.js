fd2df5c2ed775bc74d6328d542336d45e9fc12c8
wzrdk3lly <dkmk1149@gmail.com>
2022-09-03 12:46:23 -0400

    Fix code formatted

--- src/index.js
+++ src/index.js
@@ -1,55 +1,55 @@
 // Initialize game board
 const createBoard = () => {
-  let boardArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
-  console.log("board array has been created ");
+
+  // Tic Tac Toe board as array
+  let boardArray = ["0", "1", "2",
+                    "3", "4", "5", 
+                    "6", "7", "8"];
   return boardArray;
 };
 
 const gameState = () => {
   //Set up Game state
-  let gameBoard = createBoard(); // gameboard = newArray(9)
+  let gameBoard = createBoard(); 
 
   // Create player objects
   const player1 = player("Player 1", "X");
   const player2 = player("Player 2", "O");
-
   let turn = 0;
   let gameWon = false;
   let winningPlayer = "";
 
-  // grab query selectors needed for game front-end
   let boardContainer = document.querySelector("#board");
   let announcerDisplay = document.querySelector("#announcer");
   let restartButton = document.querySelector(".restart");
 
+  //Initial player notification before they begin game
   announcerDisplay.innerText = `${player1.getName()} (${player1.getMark()}) take your turn`;
 
   const playPlayerMark = (e) => {
-
     try {
-       // Validation check to prevent double turns
-    if (isInvalidMove()) return;
+      // Validation check to prevent double turns
+      if (isInvalidMove()) return;
 
-    alternatePlayerTurn();
+      alternatePlayerTurn();
 
-    if (isTie()) {
-      announcerDisplay.innerText = "It's a Tie!";
-      disableGameBoard();
-    } else if (gameWon) {
-      announcerDisplay.innerText = `${winningPlayer} Won!`;
-      disableGameBoard();
-    }
+      if (isTie()) {
+        announcerDisplay.innerText = "It's a Tie!";
+        disableGameBoard();
+      } else if (gameWon) {
+        announcerDisplay.innerText = `${winningPlayer} Won!`;
+        disableGameBoard();
+      }
     } catch (error) {
-      alert("Mistakes were made")
+      alert("Mistakes were made");
     }
-   
 
     // Toggles between the marks that players are able to place on game board
     function alternatePlayerTurn() {
       if (turn % 2 === 0) {
         player1.takeTurn(e);
         updateGameBoardArray();
-        console.log(gameBoard);
+
         isWinner(gameBoard, player1.getMark(), player1.getName());
         // Display player turn
         announcerDisplay.innerText = `${player2.getName()} (${player2.getMark()}) take your turn`;
@@ -57,7 +57,7 @@ const gameState = () => {
       } else {
         player2.takeTurn(e);
         updateGameBoardArray();
-        console.log(gameBoard);
+
         isWinner(gameBoard, player2.getMark(), player2.getName());
         // Display player turn
         announcerDisplay.innerText = `${player1.getName()} (${player1.getMark()}) take your turn`;
@@ -66,17 +66,28 @@ const gameState = () => {
     }
 
     function isWinner(gameBoard, playerMark, playerName) {
-      const winningCombinations = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]]
+      const winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+      ];
 
-      winningCombinations.forEach(i => {
-        if (gameBoard[i[0]] === playerMark && gameBoard[i[1]] === playerMark && gameBoard[i[2]] === playerMark){
-          // console.log(`${playerMark} is the winner at ${i}`); Debug assist
+      winningCombinations.forEach((i) => {
+        if (
+          gameBoard[i[0]] === playerMark &&
+          gameBoard[i[1]] === playerMark &&
+          gameBoard[i[2]] === playerMark
+        ) {
           gameWon = true;
           winningPlayer = playerName;
         }
-        })
+      });
     }
-
 
     function isTie() {
       return turn === 9 && !gameWon;
@@ -98,10 +109,10 @@ const gameState = () => {
     }
   };
 
-  // Activates game board to allow player mark placement on mousedown 
+  // Activates game board to allow player mark placement on mousedown
   boardContainer.addEventListener("mousedown", playPlayerMark);
 
-  // reload page to restart
+  // Reload page to restart
   restartButton.addEventListener("mousedown", () => {
     window.location.reload();
   });
